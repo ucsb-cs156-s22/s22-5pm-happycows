fe27cf5bde5dfed253bb8fe73edce27e344e4fef
Joseph Ng <joseph_ng@ucsb.edu>
2022-05-31 15:10:41 -0700

    jn - added day number (I used some math on frontend instead of having backend keep track of everything so backend will have less load)

--- frontend/src/main/components/Commons/CommonsOverview.js
+++ frontend/src/main/components/Commons/CommonsOverview.js
@@ -6,7 +6,7 @@ export default function CommonsOverview({ commons }) {
         <Card data-testid="CommonsOverview">
             <Card.Header as="h5">Announcements</Card.Header>
             <Card.Body>
-                <Card.Title>Today is day {commons.day}! This game will end on {commons.endingDate.split("T")[0]} at {commons.endingDate.split("T")[1]}.</Card.Title>
+                <Card.Title>Today is day {Math.ceil((new Date().getTime() - new Date(commons.startingDate).getTime())/1000/60/60/24)}! This game will end on {new Date(commons.endingDate).toLocaleDateString()}.</Card.Title>
                 <Card.Text>Total Players: {commons.totalPlayers}</Card.Text>
             </Card.Body>
         </Card>
