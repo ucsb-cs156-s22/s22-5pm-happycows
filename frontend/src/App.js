c80ac14b17b6fff27ddf2ffd98a0f890e22b7d3f
jwangathan <jonathan_wang@umail.ucsb.edu>
2022-05-31 14:34:34 -0700

    jw - edit routings for leaderboard page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -37,9 +37,7 @@ function App() {
           hasRole(currentUser, "ROLE_ADMIN") && <Route path="/admin/editcommons/:id" element={<AdminEditCommonsPage />} />
         }
         <Route path="/play/:commonsId" element={<PlayPage />} />
-        {
-          hasRole(currentUser, "ROLE_USER") && <Route path="/leaderboard/:commonsId" element={<LeaderboardPage />} />
-        }
+        <Route path="/leaderboard/:commonsId" element={<LeaderboardPage />} />
       </Routes>
     </BrowserRouter>
   );
