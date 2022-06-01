af0585849c87985fbdf8827fdfda0a5faf697abd
Joseph Ng <joseph_ng@ucsb.edu>
2022-05-31 17:26:01 -0700

    jn - fixed frontend mutation testing

--- frontend/src/main/components/Commons/CommonsOverview.js
+++ frontend/src/main/components/Commons/CommonsOverview.js
@@ -2,11 +2,15 @@ import React from "react";
 import { Card } from "react-bootstrap";
 
 export default function CommonsOverview({ commons }) {
+    // Stryker disable all
+    const dayNumber = Math.ceil((new Date().getTime() - new Date(commons.startingDate).getTime())/86400000);
+    // Stryker enable all
+    const endingDate = new Date(commons.endingDate).toLocaleDateString();
     return (
         <Card data-testid="CommonsOverview">
             <Card.Header as="h5">Announcements</Card.Header>
             <Card.Body>
-                <Card.Title>Today is day {Math.ceil((new Date().getTime() - new Date(commons.startingDate).getTime())/1000/60/60/24)}! This game will end on {new Date(commons.endingDate).toLocaleDateString()}.</Card.Title>
+                <Card.Title>Today is day {dayNumber}! This game will end on {endingDate}.</Card.Title>
                 <Card.Text>Total Players: {commons.totalPlayers}</Card.Text>
             </Card.Body>
         </Card>
