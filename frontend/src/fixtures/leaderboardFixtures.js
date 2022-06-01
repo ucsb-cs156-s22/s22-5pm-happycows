3131a03a8017d462aaba74adce4543ba467bcca5
Aashay Parab <aashay_parab@ucsb.edu>
2022-06-01 11:10:01 -0700

    Updated fixtures file

--- frontend/src/fixtures/leaderboardFixtures.js
+++ frontend/src/fixtures/leaderboardFixtures.js
@@ -39,20 +39,51 @@ const leaderboardFixtures =
             //,"averageCowHealth": 98.0
         }
     ], 
+    
+    oneLeaderboardFiveEntries: 
+    [
+        {
+            "id": 1, 
+            "commonsId": 22, 
+            "userId": 23, 
+            "totalWealth": 150000,
+            "numOfCows": 1000
+            //,"averageCowHealth": 98.0
+        }, 
+        {
+            "id": 2, 
+            "commonsId": 22, 
+            "userId": 45, 
+            "totalWealth": 1000,
+            "numOfCows": 56
+            //,"averageCowHealth": 98.0
+        }, 
+        {
+            "id": 3, 
+            "commonsId": 22, 
+            "userId": 11, 
+            "totalWealth": 500,
+            "numOfCows": 23
+            //,"averageCowHealth": 98.0
+        }, 
+        {
+            "id": 4, 
+            "commonsId": 22, 
+            "userId": 21, 
+            "totalWealth": 56700,
+            "numOfCows": 450
+            //,"averageCowHealth": 98.0
+        }, 
+        {
+            "id": 5, 
+            "commonsId": 22, 
+            "userId": 9, 
+            "totalWealth": 10000000,
+            "numOfCows": 1000000
+            //,"averageCowHealth": 98.0
+        }
+    ]
 }; 
 
 export { leaderboardFixtures };
 
-
-// @GeneratedValue(strategy = GenerationType.IDENTITY)
-// private long id;  
-
-// @Column(name="commons_id")
-// private long commonsId;  
-
-// @Column(name="user_id")
-// private long userId;  
-
-// private double totalWealth;
-
-// private int numOfCows;
