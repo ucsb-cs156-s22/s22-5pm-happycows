f8ef7c54c89c0b09e0d579c4a0f0ba734095898f
Richard Yang <richard.ruoyu.yang@gmail.com>
2022-06-01 14:16:50 -0700

    ry - added Leaderboard table and related tests

---
+++ frontend/src/main/components/Leaderboard/LeaderboardTable.js
@@ -0,0 +1,49 @@
+import React from "react";
+import OurTable from "main/components/OurTable";
+
+export default function LeaderboardTable({ userCommonsWithId }) {
+
+    // filteredCommons contains all UserCommons passed in the input parameter, but removes
+    // several unneeded headers, such as id and commonsId
+
+    const columns = [
+        {
+            Header:'ID',
+            accessor: 'id',
+        },
+        {
+            Header:'Commons ID',
+            accessor: 'commonsId',
+        },
+        {
+            Header:'Player ID',
+            accessor: 'userId',
+        },
+        {
+            Header:'Total Wealth',
+            accessor: row => String(row.totalWealth),
+            id: 'totalWealth'
+        },
+        {
+            Header:'Number of Cows',
+            accessor: 'numOfCows',
+        },
+        /*
+        {
+            Header:'Average Cow Health',
+            accessor: row => String(row.averageCowHealth),
+            id: 'averageCowHealth'
+        },
+        */
+    ];
+
+    const testid = "LeaderboardTable";
+
+    const columnsToDisplay = columns;
+    
+    return <OurTable
+        data={userCommonsWithId}
+        columns={columnsToDisplay}
+        testid={testid}
+    />;
+};
