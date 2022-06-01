f046437af145dbd5747a4fd4e2dd9696d5142cc3
Hunter Massey <huntermassey@ucsb.edu>
2022-05-31 18:19:58 -0700

    hm - add degradation rate field to backend APIs

--- src/test/java/edu/ucsb/cs156/happiercows/controllers/CommonsControllerTests.java
+++ src/test/java/edu/ucsb/cs156/happiercows/controllers/CommonsControllerTests.java
@@ -72,6 +72,7 @@ public class CommonsControllerTests extends ControllerTestCase {
       .milkPrice(8.99)
       .startingBalance(1020.10)
       .startingDate(someTime)
+      .degradationRate(25.00)
       .build();
 
     CreateCommonsParams parameters = CreateCommonsParams.builder()
@@ -80,6 +81,7 @@ public class CommonsControllerTests extends ControllerTestCase {
       .milkPrice(8.99)
       .startingBalance(1020.10)
       .startingDate(someTime)
+      .degradationRate(25.00)
       .build();
 
     String requestBody = objectMapper.writeValueAsString(parameters);
@@ -133,6 +135,7 @@ public class CommonsControllerTests extends ControllerTestCase {
       .milkPrice(8.99)
       .startingBalance(1020.10)
       .startingDate(someTime)
+      .degradationRate(25.00)
       .build();
 
     Commons commons = Commons.builder()
@@ -141,6 +144,7 @@ public class CommonsControllerTests extends ControllerTestCase {
       .milkPrice(8.99)
       .startingBalance(1020.10)
       .startingDate(someTime)
+      .degradationRate(25.00)
       .build();
 
     String requestBody = objectMapper.writeValueAsString(parameters);
@@ -370,6 +374,7 @@ public class CommonsControllerTests extends ControllerTestCase {
         .milkPrice(8.99)
         .startingBalance(1020.10)
         .startingDate(someTime)
+        .degradationRate(25.00)
         .build();
       
       when(commonsRepository.findById(eq(2L))).thenReturn(Optional.of(c));
