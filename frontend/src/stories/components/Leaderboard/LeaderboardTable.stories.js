148b5a61076c0d073d799b3257c1a1ddd56aa1b1
Aashay Parab <aashay_parab@ucsb.edu>
2022-06-01 18:00:13 -0700

    Added LeaderboardTableStories

--- frontend/src/stories/components/Leaderboard/LeaderboardTable.stories.js
+++ frontend/src/stories/components/Leaderboard/LeaderboardTable.stories.js
@@ -1,3 +1,38 @@
+import React from 'react'; 
 
+import LeaderboardTable from "main/components/Leaderboard/LeaderboardTable";
+import leaderboardFixtures from "fixtures/leaderboardFixtures"; 
 
+export default {
+    title: 'components/Leaderboard/LeaderboardTable',
+    component: LeaderboardTable
+};
 
+const Template = (args) => {
+    return (
+        <LeaderboardTable {...args} />
+    )
+};
+
+export const Empty = Template.bind({});
+
+Empty.args = {
+    leaderboard: []
+};
+
+export const OneEntry = Template.bind({}); 
+
+OneEntry.args = {
+    leaderboard: leaderboardFixtures.oneLeaderboardOneEntry
+}
+
+export const ThreeEntries = Template.bind({}); 
+
+ThreeEntries.args = {
+    leaderboard: leaderboardFixtures.oneLeaderboardThreeEntries
+}
+
+export const FiveEntries = Template.bind({});
+FiveEntries.args = {
+    leaderboard: leaderboardFixtures.oneLeaderboardFiveEntries
+}
