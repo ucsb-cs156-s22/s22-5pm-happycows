978504ea9bc1d62a82c8e5cc01ee360882729eae
John Kirchner <j_kirchner@ucsb.edu>
2022-06-01 20:12:28 -0700

    jk - Added excpetion handler for EntityNameTaken Exception

--- src/main/java/edu/ucsb/cs156/happiercows/advice/HappierCowsAdvice.java
+++ src/main/java/edu/ucsb/cs156/happiercows/advice/HappierCowsAdvice.java
@@ -2,6 +2,9 @@ package edu.ucsb.cs156.happiercows.advice;
 
 import edu.ucsb.cs156.happiercows.errors.GenericError;
 import edu.ucsb.cs156.happiercows.errors.CoffeeException;
+import edu.ucsb.cs156.happiercows.errors.EntityNotFoundException;
+import edu.ucsb.cs156.happiercows.errors.EntityNameTakenException;
+
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -38,5 +41,29 @@ public class HappierCowsAdvice {
             );
         log.error("Exception Handler called for Coffee Exception");  
         return new ResponseEntity<>(error, HttpStatus.I_AM_A_TEAPOT);
+    }
+
+    @ExceptionHandler({ EntityNotFoundException.class })
+    public ResponseEntity<GenericError> handleEntityNotFoundException(EntityNotFoundException ex) {
+        final GenericError error = new GenericError(
+            "404", 
+            ex.getMessage(), 
+            "Entity Not Found Exception", 
+            "Requested Entity does not exist"
+            );
+        log.error("Exception Handler called for Entity Not Found Exception");  
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler({ EntityNameTakenException.class })
+    public ResponseEntity<GenericError> handleEntityNameTakenException(EntityNameTakenException ex){
+        final GenericError error = new GenericError(
+            "400",
+            ex.getMessage(),
+            "Entity Name Taken Exception",
+            "Name is already taken by another entity"
+        );
+        log.error("Exception Handler called for Entity Name Taken Exception");  
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 }
