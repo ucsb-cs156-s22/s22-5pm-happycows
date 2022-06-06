a55015b3a1ce050dfcd1332e5c5cab7da931de59
Joseph Ng <joseph_ng@ucsb.edu>
2022-06-06 00:42:11 -0700

    jn - re-added page and test

---
+++ frontend/src/tests/pages/NotFoundPage.test.js
@@ -0,0 +1,17 @@
+import { render } from "@testing-library/react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { MemoryRouter } from "react-router-dom";
+import NotFoundPage from "main/pages/NotFoundPage"; 
+
+describe("NotFoundPage test", () => {
+    const queryClient = new QueryClient();
+    test("renders without crashing", () => {
+        render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <NotFoundPage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+    });
+});
