3ac51c9deb28c061629405a6b4e11ab49e4ccf2b
Joseph Ng <joseph_ng@ucsb.edu>
2022-06-06 00:15:16 -0700

    jn - fix errors with github frontend tests

--- frontend/src/tests/pages/NotFoundPage.test.js
+++ frontend/src/tests/pages/NotFoundPage.test.js
@@ -3,9 +3,28 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
 import NotFoundPage from "main/pages/NotFoundPage"; 
 
+const mockedNavigate = jest.fn();
+jest.mock('react-router-dom', () => {
+    const originalModule = jest.requireActual('react-router-dom');
+    return {
+        __esModule: true,
+        ...originalModule,
+        Navigate: (x) => { mockedNavigate(x); return null; }
+    };
+});
+
+const mockToast = jest.fn();
+jest.mock('react-toastify', () => {
+    const originalModule = jest.requireActual('react-toastify');
+    return {
+        __esModule: true,
+        ...originalModule,
+        toast: (x) => mockToast(x)
+    };
+});
+
 describe("NotFoundPage test", () => {
     const queryClient = new QueryClient();
-
     test("renders without crashing", () => {
         render(
             <QueryClientProvider client={queryClient}>
