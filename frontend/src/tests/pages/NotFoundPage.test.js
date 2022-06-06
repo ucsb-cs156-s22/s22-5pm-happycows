97b1eaf8885bf55bceac36d8a29e68819d96526a
Joseph Ng <joseph_ng@ucsb.edu>
2022-06-06 01:07:30 -0700

    jn - fixed NotFoundPage tests

--- frontend/src/tests/pages/NotFoundPage.test.js
+++ frontend/src/tests/pages/NotFoundPage.test.js
@@ -1,11 +1,25 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
-import NotFoundPage from "main/pages/NotFoundPage"; 
+import axios from "axios";
+import AxiosMockAdapter from "axios-mock-adapter";
 
-describe("NotFoundPage test", () => {
+import NotFoundPage from "main/pages/NotFoundPage";
+import { apiCurrentUserFixtures }  from "fixtures/currentUserFixtures";
+import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
+
+describe("NotFoundPage tests", () => {
     const queryClient = new QueryClient();
-    test("renders without crashing", () => {
+    const axiosMock = new AxiosMockAdapter(axios);
+
+    beforeEach(()=>{
+        axiosMock.reset();
+        axiosMock.resetHistory();
+        axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
+        axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
+    });
+
+    test("renders correctly without crashing", async () => {
         render(
             <QueryClientProvider client={queryClient}>
                 <MemoryRouter>
@@ -13,5 +27,9 @@ describe("NotFoundPage test", () => {
                 </MemoryRouter>
             </QueryClientProvider>
         );
+
+        expect(await screen.findByText("404 Error!")).toBeInTheDocument();
+        expect(screen.getByText("The Page You Are Looking For Does Not Exist Or You Do Not Have Access!")).toBeInTheDocument();
     });
+
 });
