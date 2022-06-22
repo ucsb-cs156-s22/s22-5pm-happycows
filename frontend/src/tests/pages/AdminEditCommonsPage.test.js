00d51df27607af932fd416f6bd231162b8bc8d53
Joseph Ng <joseph_ng@ucsb.edu>
2022-06-21 18:42:01 -0700

    jn - fixed minor error with degradationRate when merging

--- frontend/src/tests/pages/AdminEditCommonsPage.test.js
+++ frontend/src/tests/pages/AdminEditCommonsPage.test.js
@@ -169,10 +169,10 @@ describe("AdminEditCommonsPage tests", () => {
                 "cowPrice": 200,
                 "milkPrice": 5,
                 "startingDate": "2022-03-07T00:00:00.000Z",
-                "degradationRate": 0.85,
                 "endingDate": "2022-03-08T00:00:00.000Z",
                 "leaderboard": true,
                 "maxCowsPerPlayer": 5,
+                "degradationRate": 0.85,
             })); // posted object
         });
     });
